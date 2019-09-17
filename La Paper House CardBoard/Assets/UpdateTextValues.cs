3a159c3d018da7fe039fb5b054e7ee5adc098063
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-09-16 23:51:44 -0300

    Android Connect With Serial

---
+++ La Paper House CardBoard/Assets/UpdateTextValues.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateTextValues : MonoBehaviour
+{
+    public manager manag;
+
+    public TextMesh text;
+
+    private void Update() {
+        text.text = manag.value;
+    }
+}
